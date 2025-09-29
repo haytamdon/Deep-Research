1f2af128a88f1c980d9b736408d5dbbedd45f928
Haitam BORQANE <haytamborquane@gmail.com>
2025-09-29 21:57:32 +0100

    testing cerebras

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from deepresearch!")
+import os
+from dotenv import load_dotenv
+from cerebras.cloud.sdk import Cerebras
 
+load_dotenv()
+
+client = Cerebras(
+  api_key=os.environ.get("CEREBRAS_API_KEY"),
+)
+
+def main():
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user", 
+                "content": "Why is fast inference important?"
+            }
+        ],
+        model="llama-4-scout-17b-16e-instruct",
+    )
+    print(chat_completion)
 
 if __name__ == "__main__":
     main()
